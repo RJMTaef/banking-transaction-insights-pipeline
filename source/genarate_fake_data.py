774387437b25d8357aecead012feb5850b22efb4
Roslan Taef <roslantaef@56:7e:ce:1a:34:cc.home>
2026-04-25 21:34:16 -0300

    Generate and save fake customer dataset to CSV

--- source/genarate_fake_data.py
+++ source/genarate_fake_data.py
@@ -13,6 +13,49 @@ RAW_DATA_PATH = "data/raw"
 
 os.makedirs(RAW_DATA_PATH, exist_ok=True)
 
+#customer data generation (not hardcoding customer data, making a system that generates random customers)
+customer_names = [
+    "Roslan Taef",
+    "Amina Rahman",
+    "Kris Potter",
+    "Di Liu",
+    "Jeff Machkintosh",
+    "Kirsis Batistia",
+    "Julio Romero",
+    "Lungi Ngidi",
+    "Robert Hernandez",
+    "Sophie Roberts"
+]
+
+countries = ["England", "Bangladesh", "Canada", "USA", "India"]
+
+risk_ratings = ["Standard", "Medium", "High"]
+
+customers = []
+
+# customer generation loop
+for i in range(1,61):
+    customer = {
+        "customer_id": f"CUST{i:03d}",
+        "name": random.choice(customer_names),
+        "country": random.choice(countries),
+        "signup_date": datetime(2021, 1, 1) + timedelta(days=random.randint(0, 1000)),
+        "risk_rating": random.choice(risk_ratings)
+    }
+
+    customers.append(customer)
+
+#converting the customer loop into table
+customers_df = pd.DataFrame(customers)
+
+customers_df.to_csv(
+    os.path.join(RAW_DATA_PATH, "customers.csv"),
+    index=False
+)
+
+print("customers.csv created successfully")
+
+
 # Output files:
 # all in data folders, named as:
 # customers.csv, accounts.csc, transactions.csv
